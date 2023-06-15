d9271ae483f6ab15a7df87db697e1a1db3cdd1fa
Peter Adams-Agresti <peteraa146@gmail.com>
2023-06-15 12:55:47 -0500

    abt to pass data through post and get (101:00)

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
-@auth.route("/logout")
+@auth.route("/logout", methods=['GET', 'POST'])
 def logout():
     return "<p>Logging out </p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sing_up():
-    return render_template("sign_up.html")
+    return render_template("sign_up.html")       
