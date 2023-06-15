43f188847b4dc0c1b458d3be3ed3f48dce19fbde
Peter Adams-Agresti <peteraa146@gmail.com>
2023-06-15 15:43:45 -0500

    my version before copying and pasting his code in

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -13,5 +13,22 @@ def logout():
     return "<p>Logging out </p>"
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
-def sing_up():
+def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("name")
+        pass1 = request.form.get("password1")
+        pass2 = request.get.form("password2")
+        if len(email) < 4:
+            flash("Email must be longer than 4 characters", category = "error")
+        elif len(firstName) < 2:
+            flash("Name must be longer than 1 character", category = "error")
+        elif pass1 != pass2:
+            flash("Your passwords must match", category = "error")
+        elif len(pass1) < 7:
+            flash("Your password must be longer than 6 characters", category = "error")
+        else:
+            flash("account created", category = "success")
+            
+
     return render_template("sign_up.html")       
